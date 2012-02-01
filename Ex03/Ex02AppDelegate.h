709b0db584d2d2f95179d84882fc72b6795cf478
NLelouche <nahuel.lelouche@threepillarglobal.com>
2012-02-01 10:57:22 -0300

    Initial Commit

---
+++ Ex03/Ex02AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Ex02AppDelegate.h
+//  Ex03
+//
+//  Created by TPG on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Ex02AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ex03/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ex03
+//
+//  Created by TPG on 2/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "Ex02AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([Ex02AppDelegate class]));
+    }
+}
